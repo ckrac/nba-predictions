d5b21983e4d82efebee7703f214533f2577a9dc1
Clark Kenneth Racadio <kennethracadio@hotmail.com>
2018-04-26 13:36:45 -0400

    Create login component

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import {BrowserRouter as Router, Link, Redirect, Route} from 'react-router-dom';
 import Auth from './modules/Auth';
 import PredictionList from './components/PredictionList';
 import RegisterForm from './components/RegisterForm';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
   constructor() {
@@ -36,6 +37,25 @@ class App extends Component {
       })
   }
 
+  handleLoginSubmit = (e, data) => {
+    e.preventDefault();
+    fetch('/login', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }).then(res => res.json())
+      .then(res => {
+        Auth.authenticateToken(res.token);
+        this.setState({
+          auth: Auth.isUserAuthenticated(),
+        });
+      }).catch(err => {
+        console.log(err);
+      })
+  }
+
   render() {
     return (
       <Router>
@@ -44,6 +64,9 @@ class App extends Component {
           <Route
             exact path="/register"
             render={() => <RegisterForm handleRegisterSubmit={this.handleRegisterSubmit} />} />
+          <Route
+            exact path="/login"
+            render={() => <LoginForm handleLoginSubmit={this.handleLoginSubmit} />} />
         </div>
       </Router>
     );
