84960a7aa0f61e35a83384ba9e04324aaf9c415c
Clark Kenneth Racadio <kennethracadio@hotmail.com>
2018-04-26 13:22:24 -0400

    Create route to register

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Link, Redirect, Route} from 'react-router-dom';
 
 import Auth from './modules/Auth';
 import PredictionList from './components/PredictionList';
+import RegisterForm from './components/RegisterForm';
 
 class App extends Component {
   constructor() {
@@ -13,11 +14,36 @@ class App extends Component {
       auth: Auth.isUserAuthenticated(),
     }
   }
+
+  handleRegisterSubmit = (e, data) => {
+    e.preventDefault();
+    fetch('/users', {
+      method: 'POST',
+      body: JSON.stringify({
+        user: data,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }).then(res => res.json())
+      .then(res => {
+        Auth.authenticateToken(res.token);
+        this.setState({
+          auth: Auth.isUserAuthenticated(),
+        });
+      }).catch(err => {
+        console.log(err);
+      })
+  }
+
   render() {
     return (
       <Router>
         <div className="App">
           <Route exact path="/predictions" render={() => <PredictionList />} />
+          <Route
+            exact path="/register"
+            render={() => <RegisterForm handleRegisterSubmit={this.handleRegisterSubmit} />} />
         </div>
       </Router>
     );
